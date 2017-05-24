d1530c6f12238f5bc0fed0db25e65b73fad6575a
riccardocannella <riccardo.cannella@studenti.unicam.it>
2017-05-24 21:57:27 +0200

    Importato il generatore dell'espressione polacca enorme

---
+++ src/FP.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class FP {
+	private Random r;
+	private FileWriter out;
+	private final int DEPTH = 22;
+
+	public static void main(String[] args) {
+		String percorso = System.getProperty("user.home") + "/Desktop/expression.txt";
+		new FP(percorso);
+		System.out.println("Fine");
+	}
+
+	FP(String percorso) {
+		r = new Random();
+		String e;
+		StringTokenizer st;
+		try {
+			out = new FileWriter(percorso);
+			e = go(DEPTH);
+			st = new StringTokenizer(e);
+			while (st.hasMoreTokens())
+				out.write(" " + st.nextToken());
+			out.close();
+		} catch (Exception f) {
+			System.exit(1);
+		}
+	}
+
+	private String go(int n) {
+		if (n > 0) {
+			switch (r.nextInt(3)) {
+			case 0:
+				return "+ " + go(n - 1) + " " + go(n - 1);
+			case 1:
+				return "- " + go(n - 1) + " " + go(n - 1);
+			case 2:
+				return "* " + go(n - 1) + " " + go(n - 1);
+			case 3:
+				return "/ " + go(n - 1) + " " + go(n - 1);
+			}
+		} else{
+			return " " + r.nextDouble();
+			}
+		return "";
+	}
+}
